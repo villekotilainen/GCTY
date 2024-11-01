678be59825aef78e22d1928ac8e8620fcb166d37
Ville Kotilainen <villekotilainen03@gmail.com>
2024-11-01 19:06:08 +0200

    Corrected application name

--- root/src/main/java/gcty/root/GCTYApplication.java
+++ root/src/main/java/gcty/root/GCTYApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RootApplication {
+public class GCTYApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RootApplication.class, args);
+		SpringApplication.run(GCTYApplication.class, args);
 	}
 
 }
